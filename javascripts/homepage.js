689663b5c7a7a3289fbe9b42363ac688d0d85ba7
po0ya <pooya.mosaddegh72@gmail.com>
2014-08-24 16:11:14 +0000

    homepage global UI with correct sidebar

--- javascripts/homepage.js
+++ javascripts/homepage.js
@@ -2,14 +2,24 @@
  * Created by po0ya on 24/08/14.
  */
 
+
 demoEntered = false;
 $(document).ready( function() {
+
     $('#error').mouseover(function(){
         if (demoEntered === false) {
-            $('.demo.sidebar').sidebar('toggle');
+
+            $('#sidebar-login').sidebar({
+                overlay: true
+            }).sidebar('toggle');
+
+
+
+
             demoEntered = true;
         }
 
     });
+
 
 });
