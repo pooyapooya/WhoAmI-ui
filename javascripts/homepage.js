169d7a337b2cb3cbfb87728a50117a6779a046ce
Milad <milad.ameri73@gmail.com>
2014-09-01 16:25:25 +0430

    Left Side bar and button completed

--- javascripts/homepage.js
+++ javascripts/homepage.js
@@ -3,11 +3,35 @@
  */
 
 
-demoEntered = false;
+var demoEntered = 0;
 $(document).ready( function() {
 
-    $('#error').mouseover(function(){
-        if (demoEntered === false) {
+    $('#leftside-button').mousedown(function(){
+
+        $('#left-sidebar-login').sidebar({
+            overlay: false,
+            exclusive: false
+
+        }).sidebar('toggle');
+
+        $('#right-sidebar-login').sidebar({
+            exclusive: false
+        }).sidebar('toggle');
+
+        if( demoEntered==0 || demoEntered == 3) {
+            demoEntered = 2;
+            $('#leftside-button').css('marginLeft' , '0px' );
+        }
+        else {
+            demoEntered = 3;
+            $('#leftside-button').css('marginLeft' , '-10px' );
+
+        }
+    });
+
+
+/*    $('#error').mouseover(function(){
+        if (demoEntered === 0) {
 
             $('#left-sidebar-login').sidebar({
                 overlay: false,
@@ -19,11 +43,29 @@ $(document).ready( function() {
                 exclusive: false
             }).sidebar('toggle');
 
+            $('#leftside-button').css('marginLeft' , '0px' );
 
-            demoEntered = true;
+            demoEntered = 1;
+        }
+
+    }).mouseout(function(){
+        if (demoEntered === 1) {
+
+            $('#left-sidebar-login').sidebar({
+                overlay: false,
+                exclusive: false
+
+            }).sidebar('toggle');
+
+            $('#right-sidebar-login').sidebar({
+                exclusive: false
+            }).sidebar('toggle');
+            $('#leftside-button').css('marginLeft' , '-10px' );
+
+
+            demoEntered = 0;
         }
 
     });
-
-
+*/
 });
