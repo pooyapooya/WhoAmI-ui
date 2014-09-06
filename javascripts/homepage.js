ef50405cca273d51a539d8313f610e5604cea363
Milad <milad.ameri73@gmail.com>
2014-09-06 23:03:50 +0430

    pare shodam

--- javascripts/homepage.js
+++ javascripts/homepage.js
@@ -6,66 +6,90 @@
 var demoEntered = 0;
 $(document).ready( function() {
 
-    $('#leftside-button').mousedown(function(){
+    $('#leftside-button').click(function() {
 
-        $('#left-sidebar-login').sidebar({
+        $('#left-sidebar').sidebar({
             overlay: false,
             exclusive: false
 
         }).sidebar('toggle');
 
-        $('#right-sidebar-login').sidebar({
+        $('#right-sidebar').sidebar({
             exclusive: false
         }).sidebar('toggle');
 
-        if( demoEntered==0 || demoEntered == 3) {
+        if (demoEntered == 0 || demoEntered == 3) {
             demoEntered = 2;
-            $('#leftside-button').css('marginLeft' , '0px' );
+            $('#leftside-button').css('marginLeft', '0px').animate(
+                {width: "9%"}, 400
+            );
+            $('#left-sidebar-button-text').css('display', 'none');
+
+            $('#rightside-button').css('marginRight' , '0px' ).animate(
+                {width:"9%"},400
+            );
+            $('#right-sidebar-button-text').css('display','none');
+
         }
         else {
             demoEntered = 3;
-            $('#leftside-button').css('marginLeft' , '-10px' );
+            $('#leftside-button').css('marginLeft', '-10px').animate(
+                {width: "16%"}, 400, 'swing', function () {
+                    $('#left-sidebar-button-text').css('display', 'inline');
+                }
+            );
+            $('#rightside-button').css('marginRight' , '-10px' ).animate(
+                {width:"16%"},400,'swing',function(){
+                    $('#right-sidebar-button-text').css('display','inline');}
+            );
 
         }
     });
 
+    $('#rightside-button').click(function(){
 
-/*    $('#error').mouseover(function(){
-        if (demoEntered === 0) {
+        $('#left-sidebar').sidebar({
+            overlay: false,
+            exclusive: false
 
-            $('#left-sidebar-login').sidebar({
-                overlay: false,
-                exclusive: false
+        }).sidebar('toggle');
 
-            }).sidebar('toggle');
+        $('#right-sidebar').sidebar({
+            exclusive: false
+        }).sidebar('toggle');
 
-            $('#right-sidebar-login').sidebar({
-                exclusive: false
-            }).sidebar('toggle');
+        if (demoEntered == 0 || demoEntered == 3) {
+            demoEntered = 2;
+            $('#leftside-button').css('marginLeft', '0px').animate(
+                {width: "9%"}, 400
+            );
+            $('#left-sidebar-button-text').css('display', 'none');
 
-            $('#leftside-button').css('marginLeft' , '0px' );
+            $('#rightside-button').css('marginRight' , '0px' ).animate(
+                {width:"9%"},400
+            );
+            $('#right-sidebar-button-text').css('display','none');
 
-            demoEntered = 1;
         }
+        else {
+            demoEntered = 3;
+            $('#leftside-button').css('marginLeft', '-10px').animate(
+                {width: "16%"}, 400, 'swing', function () {
+                    $('#left-sidebar-button-text').css('display', 'inline');
+                }
+            );
+            $('#rightside-button').css('marginRight' , '-10px' ).animate(
+                {width:"16%"},400,'swing',function(){
+                    $('#right-sidebar-button-text').css('display','inline');}
+            );
 
-    }).mouseout(function(){
-        if (demoEntered === 1) {
-
-            $('#left-sidebar-login').sidebar({
-                overlay: false,
-                exclusive: false
-
-            }).sidebar('toggle');
-
-            $('#right-sidebar-login').sidebar({
-                exclusive: false
-            }).sidebar('toggle');
-            $('#leftside-button').css('marginLeft' , '-10px' );
-
-
-            demoEntered = 0;
         }
-
     });
-*/
+
+    $('.ui.dropdown').dropdown();
+    $('#button-forgotpassword-signin').click(function(){
+        $('.ui.modal')
+            .modal('show')
+    });
+
 });
